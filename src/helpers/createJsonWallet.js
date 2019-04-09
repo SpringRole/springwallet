0d74eb0bc9543243717fd9d76d812005cee5c0b7
nervehammer <suraj.raw120@gmail.com>
2019-04-09 19:29:39 +0530

    add helpers

---
+++ src/helpers/createJsonWallet.js
@@ -0,0 +1,13 @@
+//TODO: do this after creating an HD wallet using mnemonic
+
+import { Wallet, Configs } from '@/helpers';
+export default password => {
+  const createdWallet = {};
+  const wallet = new Wallet.generate();
+  createdWallet.walletJson = wallet.toV3(password, {
+    kdf: Configs.wallet.kdf,
+    n: Configs.wallet.n
+  });
+  createdWallet.name = wallet.getV3Filename();
+  return createdWallet;
+};
---
+++ src/helpers/index.js
@@ -0,0 +1,9 @@
+import Wallet from 'ethereumjs-wallet';
+import Configs from './configs';
+import CreateJsonWallet from './createJsonWallet';
+
+export {
+  Wallet,
+  Configs,
+  CreateJsonWallet,
+};
