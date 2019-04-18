c976103619f01dccb982e65d86c7990391813402
nervehammer <suraj.raw120@gmail.com>
2019-04-18 18:45:31 +0530

    remove old code

--- src/helpers/index.js
+++
@@ -1,9 +0,0 @@
-import Wallet from 'ethereumjs-wallet';
-import Configs from './configs';
-import CreateJsonWallet from './createJsonWallet';
-
-export {
-  Wallet,
-  Configs,
-  CreateJsonWallet,
-};
